3e103c3da97d1ee99cb94b78bfedbe877e635b01
AndreyS86 <108714524+AndreyS86@users.noreply.github.com>
2025-01-25 10:31:38 +0200

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,65 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useEffect } from "react";
+import Feedback from "./components/Feedback/Feedback";
+import Options from "./components/Options/Options";
+import Notification from "./components/Notification/Notification";
+import Description from './components/Description/Description';
+import "./styles.css";
 
-function App() {
-  return <>Hello World</>;
-}
+const App = () => {
+  const [feedback, setFeedback] = useState(
+    () =>
+      JSON.parse(localStorage.getItem("feedback")) || {
+        good: 0,
+        neutral: 0,
+        bad: 0,
+      }
+  );
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const positiveFeedback = totalFeedback
+    ? Math.round((feedback.good / totalFeedback) * 100)
+    : 0;
+
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
+
+  const updateFeedback = (feedbackType) => {
+    setFeedback((prevFeedback) => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1,
+    }));
+  };
+
+  const resetFeedback = () => {
+    setFeedback({ good: 0, neutral: 0, bad: 0 });
+  };
+
+  return (
+    <div>
+      <h1>Sip Happens Café</h1>
+      <p>
+        Please leave your feedback about our service by selecting one of the
+        options below.
+      </p>
+      <Options
+        onLeaveFeedback={updateFeedback}
+        onReset={resetFeedback}
+        totalFeedback={totalFeedback}
+      />
+      {totalFeedback > 0 ? (
+        <Feedback
+          good={feedback.good}
+          neutral={feedback.neutral}
+          bad={feedback.bad}
+          totalFeedback={totalFeedback}
+          positiveFeedback={positiveFeedback}
+        />
+      ) : (
+        <Notification message="No feedback given" />
+      )}
+    </div>
+  );
+};
 
 export default App;
